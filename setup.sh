4d8831fe671b2dae1f0330a181a5c7ac4c79f9dd
Dino Daniel <dinodaniel.in@gmail.com>
2017-10-24 00:06:54 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #Get path:
 myPATH=`pwd`
-echo $myPATH/
+
+#Set the CronJob
 #crontab -l 2>/dev/null; echo "0 */6 * * * $myPATH/post_status.py" | crontab -
 
 #Set Executable permission for python and shell scripts
